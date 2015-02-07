a9fc9a7d25e3211a83085d616dd440f310beba40
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-02-07 16:20:05 +0900

    Add presence validation to sessions form

--- app/forms/sessions_form.rb
+++ app/forms/sessions_form.rb
@@ -3,7 +3,13 @@ class SessionsForm
 
   attr_accessor :organization_login_name, :user_email, :user_password
 
+  validates :organization_login_name, presence: true
+  validates :user_email,    presence: true
+  validates :user_password, presence: true
+
   def login
+    return false unless valid?
+
     false
   end
 end

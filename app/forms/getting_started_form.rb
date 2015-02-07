ab4f116f9fbf037990c72ef130573b12ce630366
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-02-07 15:39:56 +0900

    Implement Getting Started form

--- app/forms/getting_started_form.rb
+++ app/forms/getting_started_form.rb
@@ -4,7 +4,32 @@ class GettingStartedForm
   attr_accessor :organization_name, :organization_login_name
   attr_accessor :user_family_name, :user_given_name, :user_email
 
+  attr_reader :organization, :user
+
+  def initialize(attrs = {})
+    super # assign instance variables
+
+    @organization = Organization.new(
+      name: organization_name,
+      login_name: organization_login_name
+    )
+
+    @user = User.new(
+      organization: organization,
+      family_name:  user_family_name,
+      given_name:   user_given_name,
+      email:        user_email
+    )
+  end
+
   def save
-    false
+    valid? && @organization.save && @user.save
+  end
+
+  def valid?
+    organization_valid = @organization.valid?
+    user_valid = @user.valid?
+
+    organization_valid && user_valid
   end
 end

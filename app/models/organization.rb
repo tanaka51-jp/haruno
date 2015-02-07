7c78595d042fdea9dac0f53bcc323eab0102dad3
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-02-07 15:38:10 +0900

    Adjust validation error message on Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -2,7 +2,7 @@ class Organization < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 255 }
   validates :login_name,
     presence: true,
-    length: { minimum: 3, maximum: 255 },
-    format: { with: /\A[a-zA-Z]([a-zA-Z0-9_\-])*\z/ },
+    length: { minimum: 3, maximum: 255, allow_blank: true },
+    format: { with: /\A[a-zA-Z]([a-zA-Z0-9_\-])*\z/, allow_blank: true },
     uniqueness: true
 end

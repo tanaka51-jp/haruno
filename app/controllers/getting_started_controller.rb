88f8d41976de1704435f765fb32fdc68293de6a1
Koichi TANAKA <tanaka51.jp@gmail.com>
2015-02-07 09:55:17 +0900

    Add getting started page

---
+++ app/controllers/getting_started_controller.rb
@@ -0,0 +1,7 @@
+class GettingStartedController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
+
+  get  'getting_started', to: 'getting_started#new'
+  post 'getting_started', to: 'getting_started#create'
 end
